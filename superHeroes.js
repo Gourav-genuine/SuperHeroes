5c0581fa7c73834e4bec6f0657294f6f0cfbdf75
Gourav Kashyap <okaygourav@gmail.com>
2023-11-13 20:04:56 +0530

    made modular using revealing pattern.

--- superHeroes.js
+++ superHeroes.js
@@ -1,26 +1,28 @@
-let publicKey = "b5271717dc6929f46119ac271a16792a";
-// Your private key
-// c5f5521bdc7991680d36f6999052b731af5bcc78
-var characters = [];
-// 9c086e347a478c1e16ae237216a23c42
-// console.log(md5("1914c5f5521bdc7991680d36f6999052b731af5bcc78b5271717dc6929f46119ac271a16792a").toString())
-var fetchCharacters = async () => {
-  try {
-    await fetch(
-      `https://gateway.marvel.com/v1/public/characters?ts=1914&apikey=b5271717dc6929f46119ac271a16792a&hash=9c086e347a478c1e16ae237216a23c42`
-    )
-      .then((response) => response.json())
-      .then((data) => {
-        // console.log(data.data.results);
-        characters = data.data.results;
-      });
-  } catch (error) {
-    console.log(error);
-  }
-};
+var superHeroModule = (function superHeroInit() {
+  var a = 10;
+  let publicKey = "b5271717dc6929f46119ac271a16792a";
+  // Your private key
+  // c5f5521bdc7991680d36f6999052b731af5bcc78
+  var characters = [];
+  // 9c086e347a478c1e16ae237216a23c42
+  // console.log(md5("1914c5f5521bdc7991680d36f6999052b731af5bcc78b5271717dc6929f46119ac271a16792a").toString())
+  var fetchCharacters = async () => {
+    try {
+      await fetch(
+        `https://gateway.marvel.com/v1/public/characters?ts=1914&apikey=b5271717dc6929f46119ac271a16792a&hash=9c086e347a478c1e16ae237216a23c42`
+      )
+        .then((response) => response.json())
+        .then((data) => {
+          // console.log(data.data.results);
+          characters = data.data.results;
+        });
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
-let heroCardElement = (hero) => {
-  return `
+  let heroCardElement = (hero) => {
+    return `
     <div class="hero-card">
       <img class="hero-card_thumbnail" src=${
         hero.thumbnail.path + "." + hero.thumbnail.extension
@@ -31,21 +33,33 @@ let heroCardElement = (hero) => {
       </div>
     </div>
   `;
-};
+  };
 
-try {
-  fetchCharacters().then(async () => {
-    for (let hero of characters) {
-      console.log(hero);
-      let heroCard = heroCardElement(hero);
-      document.getElementById("homepage_body").innerHTML += heroCard;
-    }
-  });
-} catch (error) {}
+  var hello = () => {
+    console.log('hello')
+  }
+
+  var initialise = () => {
+    try {
+      fetchCharacters().then(async () => {
+        for (let hero of characters) {
+          let heroCard = heroCardElement(hero);
+          document.getElementById("homepage_body").innerHTML += heroCard;
+        }
+      });
+    } catch (error) {}
+  };
+
+  return {
+    hello: hello,
+    initialise: initialise,
+    a: a
+  };
+})();
 
 var searchCharaters = () => {
   let searchTerm = document.getElementById("searchField").innerText;
-  console.log(searchTerm)
+  console.log(searchTerm);
   let filteredChars = characters.filter((char) => {
     return char.name.toLowerCase().includes(searchTerm.toLowerCase());
   });
@@ -61,7 +75,7 @@ document.getElementById("search").addEventListener("input", () => {
   searchCharaters();
 });
 // Add submit event listener for search form
-document.getElementById("search-form").addEventListener("submit", (e) => {
-  e.preventDefault();
-  searchCharaters();
-});
+// document.getElementById("search-form").addEventListener("submit", (e) => {
+//   e.preventDefault();
+//   searchCharaters();
+// });
